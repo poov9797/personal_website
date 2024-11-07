0a1a65bf5483d941a2a9202c1d7ac4cec03699ad
poo9797 <poovarasan9797@gmail.com>
2024-11-07 11:22:00 +0000

    yesevert "Updates"
    This reverts commit 78193339b55f5d8fd9819df6faf713f882761150.

---
+++ src/components/footer/Footer.js
@@ -0,0 +1,28 @@
+import React, {useContext} from "react";
+import "./Footer.scss";
+import {Fade} from "react-reveal";
+import emoji from "react-easy-emoji";
+import StyleContext from "../../contexts/StyleContext";
+
+export default function Footer() {
+  const {isDark} = useContext(StyleContext);
+  return (
+    <Fade bottom duration={1000} distance="5px">
+      <div className="footer-div">
+        <p className={isDark ? "dark-mode footer-text" : "footer-text"}>
+          {emoji("❤️ by Poov")}
+        </p>
+        <p className={isDark ? "dark-mode footer-text" : "footer-text"}>
+          Website theme available on {" "}
+          <a
+            href="https://github.com/poov9797"
+            target="_blank"
+            rel="noreferrer"
+          >
+            MyGitHub
+          </a>
+        </p>
+      </div>
+    </Fade>
+  );
+}
